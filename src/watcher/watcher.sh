ffb71d5e8ba840eca0eb704d6c691ad286c1551b
Tobias Grasse <tg@glancr.de>
2020-09-08 13:20:09 +0200

    fix: Improve watcher script
    Add a wait condition in case the wayland socket is not ready yet. Using the DELETE_SELF event on the socket itself lead to race conditions where inotifywait seemed to listen, but probably to an outdated inode. The sleep delay should ensure that the browser is restarted after mir-kiosk is up again.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/watcher/watcher.sh
+++ src/watcher/watcher.sh
@@ -1,5 +1,14 @@
 #! /bin/sh -e
 
-while inotifywait -e delete /run/user/0/wayland-0; do
-    snapctl restart "$SNAP_NAME".browser
+while test ! -e /run/user/0/wayland-0; do
+    echo "Wayland socket not available, waiting one second"
+    sleep 1
+done
+
+inotifywait --event delete --format '%f' /run/user/0 | while IFS=' ' read -r file; do
+    if test "$file" = "wayland-0"; then
+        echo "Wayland socket was deleted, restarting browser service in 2 seconds"
+        sleep 2
+        snapctl restart "$SNAP_NAME".browser
+    fi
 done
