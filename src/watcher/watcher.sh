05dd098aa37f0505525e0c07331989feb9fb9944
Tobias Grasse <tg@glancr.de>
2020-09-09 07:07:47 +0200

    fix: Increase wait time if wayland socket is missing
    Waiting for half a second spams the log in case of a mir-kiosk refresh; two seconds is fine as it’s still quite fast after the socket is available again.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/watcher/watcher.sh
+++ src/watcher/watcher.sh
@@ -10,7 +10,7 @@ inotifywait --event delete --format '%f' /run/user/0 | while IFS=' ' read -r fil
         echo "Wayland socket was deleted, restarting browser service in 2 seconds"
         while test ! -e /run/user/0/wayland-0; do
             echo "Wayland socket not yet available"
-            sleep 0.5
+            sleep 2
         done
         snapctl restart "$SNAP_NAME".browser
     fi
